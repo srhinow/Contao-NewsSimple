24e224f6a0b52d37e289ce0950e86ca584c89df1
Felix Pfeiffer <info@felixpfeiffer.com>
2014-03-25 10:37:40 +0100

    Add english language and clean up language files

---
+++ languages/en/tl_news.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * Core translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ * 
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao/language/de/
+ * 
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+$GLOBALS['TL_LANG']['tl_news']['newsText'][0] = 'Newstext';
+$GLOBALS['TL_LANG']['tl_news']['newsText'][1] = 'The newstext is shown in the news reader. You do not need content elements to add just a simple text.';
+
+$GLOBALS['TL_LANG']['tl_news']['newstext_legend'] = 'Newstext';
---
+++ languages/en/tl_settings.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * Core translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ * 
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao/language/de/
+ * 
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+$GLOBALS['TL_LANG']['tl_settings']['newsSimpleNoElements'][0] = 'No content elements in news.';
+$GLOBALS['TL_LANG']['tl_settings']['newsSimpleNoElements'][1] = 'Hides the possibility to add content elements to news articles.';
+
+$GLOBALS['TL_LANG']['tl_settings']['simpleNews_legend'] = 'Simple-News';
